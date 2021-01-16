5bb3aafb5ddeb3be1bad1f0b9e80cdb27275d089
Oleh <oleg.2014@gmail.com>
2021-01-16 03:45:04 +0200

    check if border defined; add docs

--- lib/color_tile.dart
+++ lib/color_tile.dart
@@ -8,22 +8,25 @@ class ColorTile extends StatefulWidget {
 }
 
 class ColorTileState extends State<ColorTile> {
+  /// Define color of background and text border (if presented).
   Color color = CreateNewColor.favorite;
 
   @override
   Widget build(BuildContext context) {
-    return new GestureDetector(
+    // Use [GestureDetector] to detect gestures.
+    return GestureDetector(
         onTap: () {
           setState(() {
             color = CreateNewColor.random();
           });
         },
-        child: new Container(
+        child: Container(
             color: color,
             child: Center(
                 child: BorderedText(
               'Hey there',
-              border: new BorderStyle(
+              border: BorderStyle(
+                size: 4,
                 color: CreateNewColor.inverse(color),
               ),
             ))));
@@ -31,67 +34,92 @@ class ColorTileState extends State<ColorTile> {
 }
 
 class BorderedText extends StatelessWidget {
+  /// Creates a text widget with borders.
+  ///
+  /// The [data] parameter must not be null.
+  ///
+  /// If the [border] argument is null, the text will be without borders.
   const BorderedText(
     this.data, {
-    Key key,
-    this.size = 60,
-    this.color = Colors.white,
-    @required this.border,
-  }) : super(key: key);
+    this.style = const TextStyle(fontSize: 60, color: Colors.white),
+    this.border,
+  }) : assert(
+          data != null,
+          'A non-null String must be provided to a BorderedText widget.',
+        );
 
-  final BorderStyle border;
-
+  /// The text to display.
   final String data;
-  final double size;
-  final Color color;
+
+  /// If non-null, the style to use for this text.
+  final TextStyle style;
+
+  /// If non-null, borders will be displayed around the text.
+  final BorderStyle border;
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: <Widget>[
-        // Stroked text as border.
-        Text(
-          data,
-          style: TextStyle(
-            fontSize: size,
-            foreground: Paint()
-              ..style = PaintingStyle.stroke
-              ..strokeWidth = border.size
-              ..color = border.color,
+    // If border not presented return simple Text Widget.
+    if (this.border == null) {
+      return Text(
+        data,
+        style: style,
+      );
+    } else {
+      return Stack(
+        children: <Widget>[
+          // Stroked text as border.
+          Text(
+            data,
+            style: TextStyle(
+              fontSize: style.fontSize,
+              foreground: Paint()
+                ..style = PaintingStyle.stroke
+                ..strokeWidth = border.size
+                ..color = border.color,
+            ),
           ),
-        ),
-        // Solid text as fill.
-        Text(
-          data,
-          style: TextStyle(
-            fontSize: size,
-            color: color,
+          // Solid text as fill.
+          Text(
+            data,
+            style: style,
           ),
-        ),
-      ],
-    );
+        ],
+      );
+    }
   }
 }
 
+/// Wrap parameters that define text border in ``BorderedText`` class.
 class BorderStyle {
+  /// Creates a combination of border parameters.
   const BorderStyle({
-    this.size = 4,
+    @required this.size,
     @required this.color,
   });
 
+  /// Used in strokeWidth in ``Paint`` foreground object and represent size of
+  /// border.
   final double size;
+
+  /// Used in color in ``Paint`` foreground object and represent color of
+  /// border.
   final Color color;
 }
 
+/// Class wrap color related operations.
 class CreateNewColor {
+  /// Return my favorite color.
   static Color get favorite {
     return Colors.deepPurple.shade500;
   }
 
+  /// Return some random color.
   static Color random() {
     return Color(Random().nextInt(0xffffffff)).withAlpha(0xff);
   }
 
+  /// Return inverse color by substituting it from white.
   static Color inverse(Color origin) {
     return Color(0xffffffff - origin.value).withAlpha(0xff);
   }
